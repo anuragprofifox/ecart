7b7d31156de7aff8d9d1534b97d83252928f55f5
anuragprofifox <45478290+anuragprofifox@users.noreply.github.com>
2019-07-12 18:39:34 +0530

    Update index.php

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Home | E-Shopper</title>
+    <title>Home | Humarashop</title>
     <link href="<?php echo base_url(); ?>assets/web/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/web/css/font-awesome.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/web/css/prettyPhoto.css" rel="stylesheet">
